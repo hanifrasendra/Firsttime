9c47552b04d08f247d388c9bd024edb32ddfc820
hanifrasendra <133474723+hanifrasendra@users.noreply.github.com>
2026-01-13 15:14:00 +0700

    ""

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,4 @@
 import Navigator from "./Navigator.jsx";
-import Stiker06 from 'public/STIKER-06 6.png';
-import Stiker02 from 'public/STIKER-02 2.png';
 
 const Hero = () => {
 
@@ -9,23 +7,23 @@ const Hero = () => {
         <div className={"mb-[109px]"}>
             <div className={"absolute z-0 top-0 overflow-hidden"}>
                 <div className={"flex animate-[scrollRight_30s_linear_infinite]"}>
-                    <img src={Stiker06} alt="" />
-                    <img src={Stiker06} alt="" />
-                    <img src={Stiker06} alt="" />
-                    <img src={Stiker06} alt="" />
-                    <img src={Stiker06} alt="" />
+                    <img src="/STIKER-06-6.png" alt="" />
+                    <img src="/STIKER-06-6.png" alt="" />
+                    <img src="/STIKER-06-6.png" alt="" />
+                    <img src="/STIKER-06-6.png" alt="" />
+                    <img src="/STIKER-06-6.png" alt="" />
                 </div>
                 <div className={"flex animate-[scrollLeft_30s_linear_infinite]"}>
-                    <img src={Stiker06} alt="" />
-                    <img src={Stiker06} alt="" />
-                    <img src={Stiker06} alt="" />
-                    <img src={Stiker06} alt="" />
-                    <img src={Stiker06} alt="" />
+                    <img src="/STIKER-06-6.png" alt="" />
+                    <img src="/STIKER-06-6.png" alt="" />
+                    <img src="/STIKER-06-6.png" alt="" />
+                    <img src="/STIKER-06-6.png" alt="" />
+                    <img src="/STIKER-06-6.png" alt="" />
                 </div>
             </div>
             <div className={"flex justify-center items-center flex-col mx-[10%] relative z-2"}>
                 <div className={"mb-[42px]"}>
-                    <img src={Stiker02} alt="" />
+                    <img src="public/STIKER-02 2.png" alt="" />
                 </div>
                 <h1 className={"text-[96px] text-center text-[rgba(0,121,79,1)] font-black mb-5"}><span className={"font-normal italic"}>Design</span> That Makes Your Brand <span className={"font-thin italic"}>Remembered</span></h1>
                 <p className={"text-[rgba(0,121,79,1)] text-[24px] mb-[48px]"}><span className={"text-[36px] italic"}>Not just pretty visuals,</span> We design with <span className={"font-bold"}>purpose, clarity, and strategy</span>, so your brand actually works !</p>
